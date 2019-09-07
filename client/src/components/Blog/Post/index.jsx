1faabddede665b5eff57779d4a199efad147ff74
Jurong <huang.jr54@gmail.com>
2019-09-07 22:04:27 +1000

    minimal page with fillers

--- client/src/components/Blog/Post/index.jsx
+++ client/src/components/Blog/Post/index.jsx
@@ -5,7 +5,7 @@ class Post extends React.Component {
     return (
       <div className="blog-post">
         <h2 className="blog-post-title">Sample blog post</h2>
-        <p className="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+        <p className="blog-post-meta">January 1, 2014</p>
         <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
         <hr></hr>
         <blockquote>
--- client/src/components/Home/header.jsx
+++ client/src/components/Home/header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Map } from '../../components/Blog';
 
 export class Header extends React.Component {
   render() {
@@ -6,15 +7,18 @@ export class Header extends React.Component {
       <div className="container">
         <header className="blog-header py-3">
           <div className="row flex-nowrap justify-content-between align-items-center">
-            <div className="col-4 pt-1">
-            </div>
-            <div className="col-4 text-center">
+            <div className="col-12 text-center">
               <a className="blog-header-logo text-dark" href="/">The green light</a>
-            </div>
-            <div className="col-4 d-flex justify-content-end align-items-center">
             </div>
           </div>
         </header>
+        <div class="nav-scroller py-1 mb-2">
+          <nav class="nav d-flex">
+            <a class="p-2 text-muted" href="#">Tag 1</a>
+            <a class="p-2 text-muted" href="#">Tag 2</a>
+          </nav>
+        </div>
+        <Map />
       </div>
     );
   }
