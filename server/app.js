497d83e1e09d503dd8d237fa8892c7d18482e18f
hjurong <huang.jr54@gmail.com>
2021-04-24 10:05:58 +1000

    commit local changes to server side code

--- server/app.js
+++ server/app.js
@@ -2,14 +2,14 @@
  * backend node app
  */
 
-const path = require('path');
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const createError = require('http-errors');
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const createError = require("http-errors");
 
 // local modules
-const indexRouter = require('./routes');
+const indexRouter = require("./routes");
 
 // Create Express application
 const app = express();
@@ -17,23 +17,23 @@ const app = express();
 // Install Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
-app.use(cors()); 
+app.use(cors());
 
 // Path routing
 app.use(indexRouter);
 
 // Error handling
-app.use(function(req, res, next) {
-  // catch 404 and forward to error handler
-  next(createError(404));
+app.use(function (req, res, next) {
+    // catch 404 and forward to error handler
+    next(createError(404));
 });
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.json('error');
+app.use(function (err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.json("error");
 });
 
 module.exports = app;
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,62 +1,66 @@
 /*
  */
-'use strict';
+"use strict";
 
-const http = require('http');
-const assert = require('assert');
-const mysql = require('mysql');
-const appSettings = require('../config/app-settings');
-const logging = require('../utils/logging');
-const logger = logging.getLogger('app::utils');
+const http = require("http");
+const assert = require("assert");
+const mysql = require("mysql");
+const mongoClient = require("mongodb").MongoClient;
+const appSettings = require("../config/app-settings");
+const logging = require("../utils/logging");
+const logger = logging.getLogger("app::utils");
 
 /**
  * Write the response from the server.
- * 
+ *
  * @param {IncomingMessage} response - the response object from the HTTP request callback
  * @param {ServerResponse} responseMessage - the message to write as a simple string
  * @param {number} statusCode - the HTTP status code for the response
  */
 function writeServerResponse(response, responseMessage, statusCode) {
-   logger.debug(responseMessage, `writeServerResponse(${statusCode})`);
-   response.statusCode = statusCode;
-   response.write(responseMessage);
-   response.end();
+    logger.debug(responseMessage, `writeServerResponse(${statusCode})`);
+    response.statusCode = statusCode;
+    response.write(responseMessage);
+    response.end();
 }
 
 /**
  * Write the response from the server.
- * 
+ *
  * @param {IncomingMessage} response - the response object from the HTTP request callback
  * @param {ServerResponse} responseJson - the message to write as a simple string
  * @param {number} statusCode - the HTTP status code for the response
  */
 function writeServerJsonResponse(response, responseJson, statusCode) {
-    logger.debug(JSON.stringify(responseJson), `writeServerJsonResponse(${statusCode})`);
-    response.setHeader('Content-Type', 'application/json');
+    logger.debug(
+        JSON.stringify(responseJson),
+        `writeServerJsonResponse(${statusCode})`
+    );
+    response.setHeader("Content-Type", "application/json");
     response.status(statusCode).send(responseJson);
 }
 
 /**
  * Processes a chunk of data using the specified handler for
  * each row, which is specific to a particular table.
- * 
+ *
  * @param {String} chunk - the chunk of data to be transformed
- * 
+ *
  * @return {array} Array of field arrays. Each element in the returned
  * array is an array of fields that represent the data for a
  * single row to be written.
  */
 function transformChunkIntoLines(chunk) {
-    let ret = { fieldsArray: [], leftOvers: ''};
+    let ret = { fieldsArray: [], leftOvers: "" };
     // Split up the chunk into lines
     let lines = chunk.split(/\r?\n/);
     // Process all lines
-    for (let aa = 0; aa < lines.length-1; aa++) {
+    for (let aa = 0; aa < lines.length - 1; aa++) {
         // Split up the line based on delimiter (| symbol, which NEVER occurs in the input data, by design)
-        let fields = lines[aa].split('|');
+        let fields = lines[aa].split("|");
         ret.fieldsArray.push(fields);
     }
-    ret.leftOvers = lines[lines.length-1];
+    ret.leftOvers = lines[lines.length - 1];
     // Return the array of field arrays
     return ret;
 }
@@ -65,20 +69,28 @@ function transformChunkIntoLines(chunk) {
  * A better assert.equal() - if the expected value does
  * not match the actual value, a meaningful message is
  * returned.
- * 
+ *
  * @param {Object} actual - the actual value
  * @param {Object} expected - the expected value
  */
 function assertEqual(actual, expected) {
-    assert.equal(actual, expected, `Assert failed: actual => ${actual}, expected => ${expected}`);
+    assert.equal(
+        actual,
+        expected,
+        `Assert failed: actual => ${actual}, expected => ${expected}`
+    );
 }
 
 var db;
 
 function getDatabase() {
-    if (typeof db === 'undefined') {
+    if (typeof db === "undefined") {
         // switch here if other db are supported
-        db = mysqldb_init();
+        if (appSettings.backingdb == "mysql") {
+            db = mysqldb_init();
+        } else if (appSettings.backingdb == "mongo") {
+            db = mongodb_init();
+        }
     }
     return db;
 }
@@ -89,31 +101,35 @@ function getDatabase() {
  */
 function mysqldb_init() {
     var connection = mysql.createConnection({
-      host     : '127.0.0.1',
-      user     : appSettings.mysqlusr,
-      password : appSettings.mysqlpwd,
-      database : appSettings.mysqldbname,
+        host: "127.0.0.1",
+        user: appSettings.mysqlusr,
+        password: appSettings.mysqlpwd,
+        database: appSettings.mysqldbname,
     });
-    
-    connection.connect(function(err) {
-      if (err) {
-        logger.error('[mysqldb_init] error connecting: ' + err.stack);
-        return;
-      }
-      logger.info('connected to mysql server');
-      process.on('exit', (code) => {
-        logger.info(`closing connection to mysql server, exit code ${code}`);
-        connection.end();
-      });
+
+    connection.connect(function (err) {
+        if (err) {
+            logger.error("[mysqldb_init] error connecting: " + err.stack);
+            return;
+        }
+        logger.info("connected to mysql server");
+        process.on("exit", (code) => {
+            logger.info(
+                `closing connection to mysql server, exit code ${code}`
+            );
+            connection.end();
+        });
     });
     return connection;
-};
+}
+
+function mongodb_init() {}
 
 /**
  * Helper function:
  * Make a call to the specified requestPath, and when the
  * results are done, invoke the callback.
- * 
+ *
  * @param {String} requestMethod - the HTTP method (GET, POST, etc)
  * @param {String} requestPath - the request path (e.g., /lists, /items, etc)
  * @param {String} postData - a JSON string (must be well-formed) containing any
@@ -122,18 +138,18 @@ function mysqldb_init() {
  * have been received from the remote server
  */
 function httpRequest(requestMethod, requestPath, postData, resultsCallback) {
-    let options = '';
-    if (requestMethod == 'GET') {
+    let options = "";
+    if (requestMethod == "GET") {
         options = `http://${appSettings.server_host}:${appSettings.server_listen_port}${requestPath}`;
         http.get(options, function requestCallback(response) {
-            let data = '';
-            response.on('data', (chunk) => {
+            let data = "";
+            response.on("data", (chunk) => {
                 data += chunk;
             });
-            response.on('end', () => {
+            response.on("end", () => {
                 resultsCallback(null, data);
             });
-            response.on('error', (err) => {
+            response.on("error", (err) => {
                 resultsCallback(err, null);
             });
         });
@@ -145,19 +161,19 @@ function httpRequest(requestMethod, requestPath, postData, resultsCallback) {
             path: encodeURI(requestPath),
             method: requestMethod,
             headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': Buffer.byteLength(postData)
-              }
+                "Content-Type": "application/json",
+                "Content-Length": Buffer.byteLength(postData),
+            },
         };
         let req = http.request(options, function requestCallback(response) {
-            let data = '';
-            response.on('data', (chunk) => {
+            let data = "";
+            response.on("data", (chunk) => {
                 data += chunk;
             });
-            response.on('end', () => {
+            response.on("end", () => {
                 resultsCallback(null, data);
             });
-            response.on('error', (err) => {
+            response.on("error", (err) => {
                 resultsCallback(err, null);
             });
         });
